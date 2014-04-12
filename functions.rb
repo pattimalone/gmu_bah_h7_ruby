82cee3e044831998f22176600bc46fc5fee22524
pattimalone <malone_patti@bah.com>
2014-04-12 15:23:38 -0400

    Update functions.rb

--- functions.rb
+++ functions.rb
@@ -35,6 +35,42 @@ def sum_only_numbers(an_array)
 end
 
 # For i from 1 to 100, return an array.
+Sum all the numbers in the array
+def sum_only_numbers(an_array)
+  total+0
+  an_array.map do |item|
+    if item.is_a?  Numeric
+      total+=item
+    end
+  end
+  return total
+end
+
+The elements in the array should follow the this algorithm:
+If i is a multiple of 3, the element is 'Fizz'
+If i is a multiple of 5, the element is 'Buzz'
+If i is a multiple of 3 and 5, the element is 'FizzBuzz'
+Otherwise, the element is value of i
+For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz',...]
+FizzArray=Array.new
+100.time do |thisNum}
+if thisNum % 3==0 && thisNum %5==0
+  fizzArray[thisNum]="FizzBuzz"
+  elsif thisNum %3==0
+  fizzArray[thisNum]="Fizz"
+  elsif thisnum %5==0
+  fizzArray[thisNum]="Buzz"
+else
+  fizzArray[thisNum]=thisNum
+end
+end return fizzArray
+end
+Uncomment each of these to test your functions
+puts revers9[3,6, 'dog']).inpect
+puts histogram('The Quick Brown Fox").inspect
+puts sum_only_numbers [4, 'foo', {}, 27, :rain, 3.14}
+puts fizzbuzz.join('\n")
+
 
 #
 
