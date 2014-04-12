7476ed797deb7f9fee180d1a7b8a19af4c017d33
pattimalone <malone_patti@bah.com>
2014-04-12 15:30:44 -0400

    Update functions.rb

--- functions.rb
+++ functions.rb
@@ -61,7 +61,7 @@ if thisNum % 3==0 && thisNum %5==0
   fizzArray[thisNum]="FizzBuzz"
   elsif thisNum %3==0
   fizzArray[thisNum]="Fizz"
-  elsif thisnum %5==0
+  elsif thisNum %5==0
   fizzArray[thisNum]="Buzz"
 else
   fizzArray[thisNum]=thisNum
@@ -70,8 +70,8 @@ end return fizzArray
 end
 Uncomment each of these to test your functions
 puts revers9[3,6, 'dog']).inpect
-puts histogram('The Quick Brown Fox").inspect
-puts sum_only_numbers [4, 'foo', {}, 27, :rain, 3.14}
+puts histogram('The Quick Brown Fox').inspect
+puts sum_only_numbers {4, 'foo', {}, 27, :rain, 3.14}
 puts fizzbuzz.join('\n")
 
 
