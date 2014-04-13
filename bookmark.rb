ba4a5fd0254a419d777d7c53f56343d72ca99524
pattimalone <malone_patti@bah.com>
2014-04-12 23:28:43 -0400

    Create bookmark.rb

---
+++ bookmark.rb
@@ -0,0 +1,19 @@
+clas bookmark
+attr accessor :url, :title
+attr.reader :last_visited
+
+def initialize(url, title)
+  @url = url
+  @title = title
+  end
+  attr_reader last-visited(visit!)
+  @last-visited = last-vissted
+  end
+  
+  def visit!
+  @last-visted = Time.now
+  end
+  
+  
+  end
+  
